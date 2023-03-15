071ba54bb5dd4cdd114c5141a20a7e21998af225
Jacob Park <jacobypark122@gmail.com>
2023-03-15 17:51:40 -0400

    done'

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self,list):
+        match_list = []
+        for word in list:
+             if sorted(self.word) == sorted(word):
+                 match_list.append(word)
+        return match_list
